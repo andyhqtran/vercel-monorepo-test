77ee6b214f9b1994719c757dbcee27d9edb6ee6a
Andy Tran <hello@andytran.me>
2020-09-28 15:33:32 -0700

    added in links

--- app/pages/dashboard.js
+++ app/pages/dashboard.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 export default function DashboardPage() {
-  return <>Dashboard page</>;
+  return (
+    <>
+      Dashboard page
+      <Link href="/" passHref>
+        <a>Go home</a>
+      </Link>
+    </>
+  );
 }
--- marketing/pages/index.js
+++ marketing/pages/index.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
-  return <>Home page</>;
+  return (
+    <>
+      Home page
+      <Link href="/" passHref>
+        <a>Go to dashboard</a>
+      </Link>
+    </>
+  );
 }
